ed489be6f8b804f22c0c3b12937d970615870c1c
Mudit Vats <muditv@gmail.com>
2019-10-17 19:33:12 -0700

    Initial checkin.

---
+++ hw1/app/src/main/java/com/example/hw/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.hw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onTask0(View view) {
+    }
+
+    public void onTask1(View view) {
+    }
+
+    public void onTask2(View view) {
+    }
+
+    public void onTask3(View view) {
+    }
+
+    public void onTask4(View view) {
+    }
+}
