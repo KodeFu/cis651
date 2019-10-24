6b701497098dddb63149463573b5e0663da2a765
Mudit Vats <muditv@gmail.com>
2019-10-23 21:06:17 -0700

    Fragment1 displays!

--- Lab3/app/src/main/java/com/example/lab3/MainActivity.java
+++ Lab3/app/src/main/java/com/example/lab3/MainActivity.java
@@ -1,14 +1,73 @@
 package com.example.lab3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentTracker {
+    private Fragment1 fragment1 = new Fragment1();
+    //private GestureDetectorCompat mDetector;
+    //private final PersonInfo pi = new PersonInfo();
+    private int next = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        loadTheFragment(fragment1);
+    }
+
+    private void loadTheFragment(Fragment f)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, f);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void fragmentVisible(String s) {
+
+    }
+
+    @Override
+    public void goNext() {
+
+    }
+
+    @Override
+    public void goBack() {
+
+    }
+
+    @Override
+    public void saveNameAndLastName(String name, String lname) {
+
+    }
+
+    @Override
+    public void saveCityAndZip(String city, String zip) {
+
+    }
+
+    @Override
+    public void saveLanguage(String language) {
+
+    }
+
+    @Override
+    public void finished() {
+
     }
 }
