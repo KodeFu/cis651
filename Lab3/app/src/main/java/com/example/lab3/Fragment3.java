0e5f02787e163f2ac54e196438cc1b9e4fc21b6e
Mudit Vats <muditv@gmail.com>
2019-10-25 06:35:29 -0700

    Save data on finish click since onDetach is not called when we start the summary view.

--- Lab3/app/src/main/java/com/example/lab3/Fragment3.java
+++ Lab3/app/src/main/java/com/example/lab3/Fragment3.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,6 +27,8 @@ public class Fragment3 extends Fragment {
         b_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                EditText language = v.findViewById(R.id.language);
+                ft.saveLanguage(language.getText().toString());
                 ft.finished();
             }
         });
@@ -54,11 +55,9 @@ public class Fragment3 extends Fragment {
 
     @Override
     public void onDetach() {
-        Log.d("xxx1", "onDetach called");
         super.onDetach();
         EditText language = v.findViewById(R.id.language);
         ft.saveLanguage(language.getText().toString());
-        Log.d("xxx2", "onDetach called");
         v = null;
     }
 
