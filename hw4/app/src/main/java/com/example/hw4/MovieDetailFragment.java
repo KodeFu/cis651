2cbfe3c8b33caa477f98e298292043e631a6ea3f
Mudit Vats <muditv@gmail.com>
2019-11-03 10:08:16 -0700

    Initial checkin.

---
+++ hw4/app/src/main/java/com/example/hw4/MovieDetailFragment.java
@@ -0,0 +1,62 @@
+package com.example.hw4;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * Use the {@link MovieDetailFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MovieDetailFragment extends Fragment {
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MovieDetailFragment newInstance(int i, String t, String y, float r, String d)  {
+        MovieDetailFragment movieDetailFragment = new MovieDetailFragment();
+        Bundle args = new Bundle();
+        args.putInt("id", i);
+        args.putString("title", t);
+        args.putString("year", y);
+        args.putFloat("rating", r);
+        args.putString("description", d);
+        movieDetailFragment.setArguments(args);
+        return movieDetailFragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Bundle args = getArguments();
+        // Inflate the layout for this fragment
+        final View v = inflater.inflate(R.layout.fragment_movie_detail, container, false);
+        ImageView imageView = v.findViewById(R.id.large_image);
+        imageView.setImageResource(args.getInt("id"));
+        EditText editText = v.findViewById(R.id.title_text);
+        editText.setText(args.getString("title"));
+        EditText yearText = v.findViewById(R.id.year_text);
+        yearText.setText(args.getString("year"));
+        RatingBar ratingBar = v.findViewById(R.id.movie_rating);
+        ratingBar.setRating(args.getFloat("rating"));
+        TextView descriptionText = v.findViewById(R.id.description);
+        descriptionText.setText(args.getString("description"));
+        return v;
+    }
+}
