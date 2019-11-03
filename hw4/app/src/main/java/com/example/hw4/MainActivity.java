f420663893bde140cf852cc435d433bd1fdd9b73
Mudit Vats <muditv@gmail.com>
2019-11-03 10:54:28 -0700

    Appbar buttons working.

--- hw4/app/src/main/java/com/example/hw4/MainActivity.java
+++ hw4/app/src/main/java/com/example/hw4/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.hw4;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolBar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolBar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about_action:
+                Log.d("onOptionsItemSelected", "about");
+                return true;
+            case R.id.task1_action:
+                Log.d("onOptionsItemSelected", "task1");
+                return true;
+            case R.id.task2_action:
+                Log.d("onOptionsItemSelected", "task2");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
