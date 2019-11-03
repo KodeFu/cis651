a089cb3bbd10878dbd147fc668ba28065de8ca2a
Mudit Vats <muditv@gmail.com>
2019-11-03 10:36:44 -0700

    Appbar / toolbar working.

--- hw4/app/src/main/java/com/example/hw4/MainActivity.java
+++ hw4/app/src/main/java/com/example/hw4/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.hw4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolBar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolBar);
     }
 }
