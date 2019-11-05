efdf7873423614efbe71e8e6ffe3b16aa0bedf23
Mudit Vats <muditv@gmail.com>
2019-11-04 18:09:59 -0700

    Added card_view.xml which are the list thumbnails. Added MyRecyclerAdapter.java which will be used to return card_views. OnListItemClickListener.java is used for something.

---
+++ hw4/app/src/main/java/com/example/hw4/OnListItemClickListener.java
@@ -0,0 +1,9 @@
+package com.example.hw4;
+
+import android.view.View;
+
+public interface OnListItemClickListener  {
+    public void onItemClick(View v, int position);
+    public void onItemLongClick(View v, int position);
+}
+
