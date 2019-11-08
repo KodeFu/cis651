4dfb869233d4038319d8daf61a050b5ff4abcb19
Mudit Vats <muditv@gmail.com>
2019-11-08 16:35:28 -0700

    Added animations.

--- lab5/app/src/main/java/com/example/lab5/MainActivity.java
+++ lab5/app/src/main/java/com/example/lab5/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.lab5;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityOptionsCompat;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +24,32 @@ public class MainActivity extends AppCompatActivity {
         ActivityOptionsCompat optionsCompat =
                 ActivityOptionsCompat.makeSceneTransitionAnimation(this, view, "newActivity");
         startActivity(intent, optionsCompat.toBundle());
+    }
+
+    public void MoveAndRotate(View view) {
+        ImageView imageView = findViewById(R.id.image_view);
+        imageView.animate().setDuration(1000).x(500).y(800).rotationYBy(720).scaleX(.4F).scaleY(.4F);
+    }
+
+    public void MoveBack(View view) {
+        ImageView imageView = findViewById(R.id.image_view);
+        imageView.animate().setDuration(1000).x(imageView.getLeft()).y(imageView.getTop()).rotationYBy(720).scaleX(1F).scaleY(1F);
+    }
+
+    public void FadeOut(View view) {
+        ImageView imageView = findViewById(R.id.image_view);
+        imageView.animate().setDuration(1000).alpha(0F);
+    }
+
+    public void FadeIn(View view) {
+        ImageView imageView = findViewById(R.id.image_view);
+        imageView.animate().setDuration(1000).alpha(1F);
+    }
+
+    public void XMLAnimator(View view) {
+        ImageView imageView = findViewById(R.id.image_view);
+        AnimatorSet spinSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.animator);
+        spinSet.setTarget(imageView);
+        spinSet.start();
     }
 }
