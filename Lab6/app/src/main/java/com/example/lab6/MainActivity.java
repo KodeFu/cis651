b725ff06a60e9749575ade4e19a17a248ef600b5
Mudit Vats <muditv@gmail.com>
2019-11-11 18:46:56 -0700

    End of Step 5.

--- Lab6/app/src/main/java/com/example/lab6/MainActivity.java
+++ Lab6/app/src/main/java/com/example/lab6/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.lab6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private MyDBHelper dbHelper;
+    private MyRecyclerAdapter adapter;
+    private String filter = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("TAG", "OnCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView)findViewById(R.id.mainRecyclerView);
+        mLayoutManager = new LinearLayoutManager(this);
+        mLayoutManager.scrollToPosition(0);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        populaterecyclerView(filter);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //fab.setOnClickListener(view);
+    }
+    private void populaterecyclerView(String filter) {
+        dbHelper = new MyDBHelper(this);
+        adapter = new MyRecyclerAdapter(dbHelper.contactList(filter), this, mRecyclerView);
+        mRecyclerView.setAdapter(adapter);
     }
 }
