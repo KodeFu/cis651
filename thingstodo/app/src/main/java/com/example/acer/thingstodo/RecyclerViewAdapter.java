ba91c599469e3e1e882ce79cc21b6925a595a73f
Mudit Vats <muditv@gmail.com>
2019-12-05 19:19:44 -0700

    Update database. Adding likes sort of working...

--- thingstodo/app/src/main/java/com/example/acer/thingstodo/RecyclerViewAdapter.java
+++ thingstodo/app/src/main/java/com/example/acer/thingstodo/RecyclerViewAdapter.java
@@ -3,11 +3,17 @@ package com.example.acer.thingstodo;
 import android.content.Context;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,23 +23,47 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
     DatabaseReference mRootReference= FirebaseDatabase.getInstance().getReference();
     DatabaseReference postsRef = mRootReference.child("posts");
-    List<Post> data=new ArrayList<Post>();
+    static List<Post> data=new ArrayList<Post>();
     Context contex;
     RecyclerView rv;
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public TextView uname;
         public TextView utext;
         public  TextView ptime;
+        public Button heartButton;
         public ViewHolder(View view) {
             super(view);
             uname=(TextView)view.findViewById(R.id.user_name);
             utext=(TextView)view.findViewById(R.id.user_text);
             ptime=(TextView)view.findViewById(R.id.post_time);
+            heartButton=(Button)view.findViewById(R.id.heart);
+
+            uname.setOnClickListener(this);
+            heartButton.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Log.d("mv", "onClick: onClick");
+            if (view.getId() == heartButton.getId()) {
+                //Toast.makeText(view.getContext(), "ITEM PRESSED = " + String.valueOf(getAdapterPosition()), Toast.LENGTH_SHORT).show();
+                Toast.makeText(view.getContext(), "ITEM PRESSED = " + view.getResources().getResourceName(view.getId()), Toast.LENGTH_SHORT).show();
+
+                int adapterPosition = getAdapterPosition();
+                assfunc(adapterPosition);
+
+            } else {
+                Toast.makeText(view.getContext(), "ROW PRESSED = " + String.valueOf(getAdapterPosition()), Toast.LENGTH_SHORT).show();
+            }
+
+            //listenerRef.get().onPositionClicked(getAdapterPosition());
         }
     }
     public RecyclerViewAdapter(Context _context){
@@ -131,6 +161,46 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @Override
     public int getItemCount() {
         return data.size();
+    }
+
+    static public void assfunc(int i)
+    {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance().getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        Post post = data.get(i);
+
+        DatabaseReference mRootReference = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference postsRef = mRootReference.child("posts");
+        DatabaseReference newPostRef = postsRef.push();
+        Map<String, Object> nodes = new HashMap<String, Object>();
+        nodes.put(post.id + "/" + user.getUid().toString(), post.id);
+        postsRef.updateChildren(nodes);
+
+        //DatabaseReference likesRef = newPostRef.child("likes");
+        //DatabaseReference likesRef = mRootReference.child("posts").child(post.id).child("likes");
+        DatabaseReference likesRef = mRootReference.child("posts").child(post.id);
+        //DatabaseReference newLikeRef = likesRef.push();
+        Map<String, Object> likeNodes = new HashMap<String, Object>();
+        //likeNodes.put(user.getUid(), "I like it!");
+
+        likeNodes.put("likes" + "/extra", "extra info");
+
+        likesRef.updateChildren(likeNodes);
+
+        /*DatabaseReference likesRef = mRootReference.child("posts").child(user.getUid().toString()).child("likes");
+        Map<String, Object> likesNodes = new HashMap<String, Object>();
+        likesNodes.put(user.getUid().toString(), "i like it!");
+        postsRef.updateChildren(likesNodes);*/
+        /*if (i==0)
+        {
+            nodes.put("-LvG0sGTsIcYeRzWfXFA" + "/stuff", "zero");
+        }
+        else
+        {
+            nodes.put("-LvIPUQkVxicwZIY12qG" + "/stuff", "oneyo");
+        }*/
+
     }
 
 }
